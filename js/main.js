d9cc4b77127b73c99dcad6e89b1618446a18aa6f
karthickuid <karthickuid@m2p.in>
2021-02-02 14:12:08 +0530

    res

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
 
     // payease expand
-
     $(".payeasecard").click(function () {
 
         $(this).addClass("active");
         if ($(".payeasecard").hasClass("active")) {
             $(".payeasecard").find(".drop-sec").slideUp();
             $(".payeasecard").find(".chk").removeClass("chked");
-            $(".payeasecard").removeClass("active")
+            $(".payeasecard").removeClass("active");
+            $(this).addClass("active");
             $(this).find(".drop-sec").slideDown();
             $(this).find(".chk").addClass("chked");
         }
     });
-
     // side nav section
-    $(".sidenav li").click(function () {
+    $(".paybx .sidenav li").click(function () {
         $(".sidenav li").removeClass("active");
         $(this).addClass("active");
 
@@ -26,7 +25,20 @@ $(document).ready(function () {
             $(".cc-form-sec").hide();
             $(".ease-form-sec").show();
         }
-    })
+    });
+
+    $(".paybx-responsive .sidenav li").click(function () {
+        $(".sidenav li").removeClass("active");
+        $(this).addClass("active");
+
+        if ($(".cc").hasClass("active")) {
+            $(".cc-form-sec").slideDown();
+            $(".ease-form-sec").slideUp();
+        } else if ($(".ease").hasClass("active")) {
+            $(".cc-form-sec").slideUp();
+            $(".ease-form-sec").slideDown();
+        }
+    });
 
 
 });
